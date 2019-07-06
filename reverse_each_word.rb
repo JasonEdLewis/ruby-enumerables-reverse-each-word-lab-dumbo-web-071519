c6845300ab5ebb8013459b482b4de9d9cda259af
Jason Lewis <jasonedlewis@gmail.com>
2019-07-06 05:04:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,11 +9,5 @@ reversed_words.join(" ")
 end
 
 def reverse_each_word(string)
-words_arr = string.split(" ")
-reversed_words =[]
-
-words_arr.each do |word|
-  reversed_words << word.reverse
-end
-reversed_words.join(" ")
+string
 end
